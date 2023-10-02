54f16be766020319403750dc7ee623b52868de1a
YuteOctober <yudthsoponvit@gmail.com>
2023-10-01 23:24:13 -0400

    .

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import ScrollToTop from './components/ScrollToTop';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <App />
       <ScrollToTop />
     </BrowserRouter>
